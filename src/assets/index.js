4d018721c73d4abf10667291459318bea047771e
Milos <milos.ribera@gmail.com>
2023-04-20 17:31:26 +0200

    remove unused file + prepare to gh-pages

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,35 +12,24 @@ import web from "./web.png";
 import android from "./tech/android.png";
 import angular from "./tech/angular.png";
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import java from "./tech/java.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
 import typescript from "./tech/typescript.png";
 
 import craon from "./company/craon.png";
 import jumbo from "./company/jumbo.png";
 import luna from "./company/luna.jpeg";
-import meta from "./company/meta.png";
 import reply from "./company/reply.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import ubi from "./company/ubi.png";
 
-import carrent from "./carrent.png";
 import expired from "./expired.png";
-import jobit from "./jobit.png";
 import jumboapp from "./jumboapp.png";
-import tripguide from "./tripguide.png";
 import uigp from "./uigp.jpg";
 
 import basket from "./hobbies/basket.jpg";
@@ -63,31 +52,20 @@ export {
   android,
   angular,
   css,
-  docker,
   figma,
   git,
   html,
   java,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
   redux,
-  tailwind,
   typescript,
-  threejs,
   ubi,
   jumbo,
   luna,
   craon,
   reply,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   uigp,
   jumboapp,
   expired,
